f913b76e43f69e6f65848771205f6700fa191357
Keith Lin <klin@varomoney.com>
2019-07-24 15:32:00 -0700

    asdf

--- test.tf
+++ test.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-005bdb005fb00e791"
   instance_type = "t2.micro"
+  subnet_id = "subnet-03c673423dbb8e6ef"
+  vpc_security_group_ids = ["sg-0869a9366b53add91"]
+
 }
 provider "aws" {
   region                  = "us-west-2"
