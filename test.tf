52da66eb281eec782f402d70c3ac0ad91f282e77
Keith Lin <klin@varomoney.com>
2019-07-24 16:40:43 -0700

    Asdf

--- test.tf
+++ test.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "example" {
   ami           = "ami-005bdb005fb00e791"
   instance_type = "t2.micro"
   subnet_id = "subnet-7e0dc735"
-  vpc_security_group_ids = ["infra-ingress-atlantis-tcp"]
+  vpc_security_group_ids = ["sg-0d08fe10ca190b46a"]
 
 }
 provider "aws" {
